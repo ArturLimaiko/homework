14cc63544daac3ad889ba64e43f00da561045e1b
Artur <alimayko91@gmail.com>
2023-09-25 14:18:10 +0300

    homework-1 done.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { HW1 } from './HW/1/HW1';
 import { HW2 } from './HW/2/HW2';
 import { HW3 } from './HW/3/HW3';
 import { HW4 } from './HW/4/HW4';
+import {UserList} from "./HW/1/UserList";
 
 function App() {
   return (
     <div className="App">
-      {/*<HW1 />*/}
+      <HW1/>
       {/*<HW2 />*/}
       {/*<HW3 />*/}
       {/*<HW4 />*/}
